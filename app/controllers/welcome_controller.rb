5ec1b42cafa391b593ccb696db962a3ffe580f7c
Thiago Nunes <thiagorinu@gmail.com>
2019-01-12 20:16:29 -0200

    Save/Load Sessions; refine user-defined filters

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
   
   def index
-    # Xplain::ResultSet.load_all.each{|rs| rs.delete}
-    session[:current_session] = Xplain::Session.new(SecureRandom.uuid, "Unnamed")
+    
+    session[:current_session] ||= Xplain::Session.new(SecureRandom.uuid, "Unnamed")
 
 
   end
